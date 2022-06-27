ce8b0d2e01a2554bb00418b44487d4310e28b846
Amir <amirmohamadrayeji@gmail.com>
2022-06-27 15:12:09 +0430

    settingup serializers files

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
 
-# Create your views here.
+from .serializers import ArticleSerializer
+from blog.models import Article
+
+class ArticleList(ListCreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
