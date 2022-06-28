e2b151901ff9752051f72214bb3950677c0bac0b
Amir <amirmohamadrayeji@gmail.com>
2022-06-28 16:03:08 +0430

    permissions settingup

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from django.contrib.auth.models import User
+from rest_framework.permissions import IsAdminUser
 
 from .serializers import ArticleSerializer, UserSerializer
 from blog.models import Article
@@ -17,8 +18,10 @@ class ArticleDetail(RetrieveUpdateDestroyAPIView):
 class UserList(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAdminUser,)
 
 
 class UserDetail(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAdminUser,)
