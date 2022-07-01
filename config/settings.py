665c20536b35eadd73b5e4f3c9d6f7a93e91688e
Amir <amirmohamadrayeji@gmail.com>
2022-07-01 20:50:20 +0430

    add tokenauthentication to project

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
     'blog',
     'api',
 ]
@@ -134,6 +135,6 @@ REST_FRAMEWORK = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
